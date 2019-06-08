5dd1d671824a9ea6ddfdcbb95579b43ff0d86e13
ilhame-gorgoma <50554730+ilhame-gorgoma@users.noreply.github.com>
2019-06-08 12:19:04 +0200

    First commit
    Ajout bulma jquery
    different components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { NewsComponent } from './components/news/news.component';
+import { ActionsComponent } from './components/actions/actions.component';
+import { UsComponent } from './components/us/us.component';
+import { DonateComponent } from './components/donate/donate.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ContactComponent,
+    NewsComponent,
+    ActionsComponent,
+    UsComponent,
+    DonateComponent
   ],
   imports: [
     BrowserModule,
