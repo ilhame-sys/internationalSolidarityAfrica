cf4c3a2c527f73f796a40426b17e19b89a45dffa
ilhame-gorgoma <50554730+ilhame-gorgoma@users.noreply.github.com>
2019-06-08 13:05:53 +0200

    Ajout routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,40 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ActionsComponent } from './components/actions/actions.component';
+import { DonateComponent } from './components/donate/donate.component';
+import { NewsComponent } from './components/news/news.component';
+import { UsComponent } from './components/us/us.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent
+  },
+  {
+    path: "contact",
+    component: ContactComponent
+  },
+  {
+    path: "actions",
+    component: ActionsComponent
+  },
+  {
+    path: "donate",
+    component: DonateComponent
+  },
+  {
+    path: "news",
+    component: NewsComponent
+  },
+  {
+    path: "us",
+    component: UsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
